68b31a8e7e306478fc4a7a55e3db4a9b733c89bf
Michael Waterfall <97835853+MichaelWaterfall@users.noreply.github.com>
2024-02-16 15:11:10 +0000

    goStore

--- script.js
+++ script.js
@@ -17,11 +17,22 @@ const monsterStats = document.querySelector("#monsterStats");
 const monsterName = document.querySelector("#monsterName");
 const monsterHealthText = document.querySelector("#monsterHealth");
 
+// initialize buttons
+button1.onclick = goStore;
+button2.onclick = goCave;
+button3.onclick = fightDragon;
+
 function goStore() {
-  console.log("Going to store.");
+  button
+  button1.innerText = "Buy 10 health (10 gold)";
+  button2.innerText = "Buy weapon (30 gold)";
+  button3.innerText = "Go to town square";
 }
 
 function goCave() {
   console.log("Going to cave.");
 }
 
+function fightDragon() {
+  console.log("Fighting dragon.");
+}
