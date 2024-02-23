f0652273d66800bd3adb6e497adaf9f4f9950057
Michael Waterfall <97835853+MichaelWaterfall@users.noreply.github.com>
2024-02-23 05:52:24 +0000

    location button text

--- script.js
+++ script.js
@@ -37,14 +37,14 @@ button2.onclick = goCave;
 button3.onclick = fightDragon;
 
 function update(location) {
-  button1.innerText = "Go to store";
-  button2.innerText = "Go to cave";
-  button3.innerText = "Fight dragon";
-  button1.onclick = goStore;
-  button2.onclick = goCave;
-  button3.onclick = fightDragon;
-  text.innerText = "You are in the town square. You see a sign that says \"Store\".";
-}
+    button1.innerText = location["button text"];
+    button2.innerText = "Go to cave";
+    button3.innerText = "Fight dragon";
+    button1.onclick = goStore;
+    button2.onclick = goCave;
+    button3.onclick = fightDragon;
+    text.innerText = "You are in the town square. You see a sign that says \"Store\".";
+  }
 
 function goTown() {
   update(locations[0]);
