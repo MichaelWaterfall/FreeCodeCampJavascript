bca45088dccff4473aa1a25506cdb19e3f93c9b2
Michael Waterfall <97835853+MichaelWaterfall@users.noreply.github.com>
2024-02-27 20:46:39 +0000

    winGame

--- script.js
+++ script.js
@@ -180,7 +180,11 @@ function attack() {
     if(health <= 0) {
         lose();
     } else if (monsterHealth <= 0) {
-        defeatMonster();
+        if(monsters[fighting] === 2) {
+            winGame();
+        } else {
+            defeatMonster();
+        }
       }
 }
 
